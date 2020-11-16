bf8efd270e374d08eaef6af0f49bc1f27cfd6d65
papillon <nobepon@gmail.com>
2020-11-16 12:33:33 +0900

    remove default template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Home />
   </div>
 </template>
 
 <script>
 import "normalize.css";
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Home,
   },
 };
 </script>
