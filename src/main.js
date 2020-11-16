7455bff500d55ad7e726dcc615a89d1bac40abad
papillon <nobepon@gmail.com>
2020-11-16 12:31:44 +0900

    add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import Vue from "vue";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
